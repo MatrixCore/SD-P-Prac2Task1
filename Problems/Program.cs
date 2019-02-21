155dc95d5a096bcc204ad56977ae360015720631
Matthew Pitcairn <G17P9403@ict.ru.ac.za>
2019-02-21 15:32:20 +0200

    Adding Program.cs

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,27 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+            Console.WriteLine(Prefix(text));
+        }
+
+        static string Prefix(string input)
+        {
+            if (input == null)
+            {
+                return null;
+            } else {
+                string result = "";
+            int len = input.Length;
+            int wordcount = input.Split(' ').Length - 1;
+
+            result = $"{len},{wordcount}{input}";
+            return result;
+            }            
         }
     }
 }
