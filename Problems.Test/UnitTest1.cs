d735c51ead714e0f1ff75981376af47052290cf9
Matthew Pitcairn <G17P9403@ict.ru.ac.za>
2019-02-21 17:01:11 +0200

    added tests

--- Problems.Test/UnitTest1.cs
+++ Problems.Test/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using Problems;
+
 
 namespace Tests
 {
@@ -7,12 +9,28 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            
         }
 
         [Test]
-        public void Test1()
+        public void TestPrefix()
         {
-            Assert.Pass();
+            Program test = new Program();
+            Assert.AreEqual("5,1:hello", test.Prefix("hello"));
+        }
+
+        [Test]
+        public void TestPrefixBlank()
+        {
+            Program test = new Program();
+            Assert.AreEqual("0,0:", test.Prefix(""));
+        }
+
+        [Test]
+        public void TestPrefixLong()
+        {
+            Program test = new Program();
+            Assert.AreEqual("27,5:what ... did you say?? ", test.Prefix("what ... did you say?? "));
         }
     }
 }
