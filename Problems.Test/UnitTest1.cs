daa0db08dcab006ed5ce75302fa612ca4d71da05
Matthew P <31068187+MatrixCore@users.noreply.github.com>
2019-02-23 14:25:05 +0200

    Fixing UnitTest1

--- Problems.Test/UnitTest1.cs
+++ Problems.Test/UnitTest1.cs
@@ -32,5 +32,26 @@ namespace Tests
             Program test = new Program();
             Assert.AreEqual("27,5:what   ...   did you say?? ", test.Prefix("what   ...   did you say?? "));
         }
+        
+        [Test]
+		public void TestPrefixNull()
+		{
+			Program test = new program();
+			Assert.AreEqual(null, test.Prefix(null));
+		}
+		
+		[Test]
+        public void TestPrefixShort()
+        {
+            Program test = new Program();
+            Assert.AreEqual("1,1:", test.Prefix("H"));
+        }
+		
+		[Test]
+        public void TestPrefixGarbage()
+        {
+            Program test = new Program();
+            Assert.AreEqual("13,3:", test.Prefix("!@#$ %^&*- =+"));
+        }
     }
 }
